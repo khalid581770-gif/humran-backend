582b11323568d955730cf25f09c1ac31229cdfab
khalid <your@email.com>
2026-04-21 02:43:59 +0300

    add ssl config

--- src/config/database.js
+++ src/config/database.js
@@ -5,6 +5,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
+  ssl: process.env.DB_SSL === 'true' ? {rejectUnauthorized: false} : false,
   host:               process.env.DB_HOST     || 'localhost',
   port:              parseInt(process.env.DB_PORT) || 3306,
   user:               process.env.DB_USER     || 'root',
